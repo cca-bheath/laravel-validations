caabf67eec468399f778c43f7230bc9eedea04e9
Rafa Rafael <rafahju@gmail.com>
2020-03-02 15:19:49 +0800

    Setup factory path to be able to use user factory and override setUpDatabase method to create user table schema

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,9 @@
 namespace CCA\LaravelValidations\Tests;
 
 use CCA\LaravelValidations\LaravelValidationsServiceProvider;
+use Illuminate\Database\Eloquent\Factory as EloquentFactory;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 abstract class TestCase extends Orchestra
@@ -10,6 +13,10 @@ abstract class TestCase extends Orchestra
     public function setUp(): void
     {
         parent::setUp();
+
+        $this->setUpDatabase();
+
+        $this->app->make(EloquentFactory::class)->load(__DIR__.'/Factories');
     }
 
     protected function getPackageProviders($app)
@@ -17,5 +24,17 @@ abstract class TestCase extends Orchestra
         return [
             LaravelValidationsServiceProvider::class,
         ];
+    }
+
+    protected function setUpDatabase()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
+            $table->string('remember_token');
+            $table->timestamps();
+        });
     }
 }
