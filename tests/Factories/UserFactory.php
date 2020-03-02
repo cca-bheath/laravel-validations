0872f501365d67b2ed143b5e48b06739eee35146
Rafa Rafael <rafahju@gmail.com>
2020-03-02 15:19:42 +0800

    Add user factory to seed user dummy data

---
+++ tests/Factories/UserFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Foundation\Auth\User;
+
+$factory->define(User::class, function(Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => Str::random(10),
+    ];
+});
